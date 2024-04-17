38ba0838275a136c21b663e5b4459f8dc9431169
kskelly03 <137232179+kskelly03@users.noreply.github.com>
2024-04-17 17:27:04 +0000

    lab 7

--- main.py
+++ main.py
@@ -17,26 +17,34 @@ def reachable(graph, start_node):
     result = set([start_node])
     frontier = set([start_node])
     while len(frontier) != 0:
-        ###TODO
-        pass
+      current_node = frontier.pop()
+      for neighbor in graph[current_node]:
+        if neighbor not in result:
+            result.add(neighbor)
+            frontier.add(neighbor)
     return result
 
-
-
-
-
 def connected(graph):
-    ### TODO
-    pass
-
-
-
+  start_node = next(iter(graph))
+  reached = reachable(graph, start_node)
+  return len(reached) == len(graph) 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
+  """
+  Returns:
+    the number of connected components in an undirected graph
+  """
+  visited_nodes = set() 
+  count = 0
+  for node in graph:
+    if node not in visited_nodes:
+      new_visited = reachable(graph, node)
+      visited_nodes.update(new_visited)
+      count += 1  
+  return count
+
+        
+        
+  
+  
 
--- test_main.py
+++ test_main.py
@@ -1,9 +1,8 @@
-from main_sol import *
+from main import *
 
 def test_reachable():
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
     assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
-
     graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
     assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
     assert sorted(reachable(graph, 'E')) == ['E', 'F', 'G']
